2c3e65ae972ea2a036771784cd85c7339c9b4eb6
rifat71 <rifatmahmud24311@gmail.com>
2023-04-09 07:13:12 +0600

    get data from json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Card from './components/Card';
+import CardItem from './cardItem.json'
 
 function App() {
+
     return <div>
         <h1 className="headerStyle">Welcome</h1>
-        <Card title="Item 1" description="First Item"/>
-        <Card title="Item 2" description="Second Item"/>
-        <Card title="Item 3" description="Third Item"/>
+        <Card title={CardItem[0].title} description={CardItem[0].description}/>
+        <Card title={CardItem[1].title} description={CardItem[1].description}/>
+        <Card title={CardItem[2].title} description={CardItem[2].description}/>
     </div>
 }
 
