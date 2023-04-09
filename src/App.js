99dd310958c41432ceaefdba5fa6558dfe373859
rifat71 <rifatmahmud24311@gmail.com>
2023-04-09 07:03:56 +0600

    update names

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Card from './components/Card';
 
 function App() {
     return <div>
-        <h1 className="headingStyle">Welcome</h1>
-        <Card titleText="Item 1" descText="First Item"/>
-        <Card titleText="Item 2" descText="Second Item"/>
-        <Card titleText="Item 3" descText="Third Item"/>
+        <h1 className="headerStyle">Welcome</h1>
+        <Card title="Item 1" description="First Item"/>
+        <Card title="Item 2" description="Second Item"/>
+        <Card title="Item 3" description="Third Item"/>
     </div>
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,12 @@
-// const text1 = "This is our very first demo project. So this is pretty cool.";
 const date = new Date().getDate();
 const month = new Date().getMonth();
 const year = new Date().getFullYear();
 
 function Card(props) {
-    const {titleText, descText} = props;
+    const {title, description} = props;
     return <div className='card'>
-        <h3 className='cardTitle'>{titleText}</h3>
-        <p className='cardDesc'>{descText}</p>
+        <h3 className='cardTitle'>{title}</h3>
+        <p className='cardDescription'>{description}</p>
         <p className='cardFooter'>{date + "/" + month + "/" + year}</p>
     </div>
 }
