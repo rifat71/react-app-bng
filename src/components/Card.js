a3a41808d1a31e270adc2382479e5826259d6f40
rifat71 <rifatmahmud24311@gmail.com>
2023-04-12 06:22:16 +0600

    Add react-icon

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,4 @@
+import { ImOffice, ImHome3, ImCalendar } from "react-icons/im";
 const date = new Date().getDate();
 const month = new Date().getMonth();
 const year = new Date().getFullYear();
@@ -6,12 +7,25 @@ const year = new Date().getFullYear();
 function Card(props) {
     const { title, description, cellPhone, picture } = props;
     return <div className='card'>
-        <img className='cardItemPic' src={picture} alt="Image loading..."/>
+        <img className='cardItemPic' src={picture} alt="" />
         <h3 className='cardTitle'>{title}</h3>
         <p className='cardDescription'>{description}</p>
-        <p className='cardDescription'>Home: {cellPhone[0]}</p>
-        <p className='cardDescription'>Office: {cellPhone[1]}</p>
-        <p className='cardFooter'>{date + "/" + month + "/" + year}</p>
+
+        <div className="icon_div">
+            <ImHome3/>
+            <p className="icon_text">{cellPhone[0]}</p>
+        </div>
+
+        <div className="icon_div">
+            <ImOffice />
+            <p className='icon_text'> {cellPhone[1]}</p>
+        </div>
+
+        <div className="icon_div">
+            <ImCalendar />
+            <p className='cardFooter icon_text'>{date + "/" + month + "/" + year}</p>
+        </div>
+        
     </div>
 }
 
